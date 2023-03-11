2733201ce00363d75c183a49714b2e8f235e2382
FraVerr <verrillo.francesca@gmail.com>
2023-03-11 11:45:23 +0100

    Added related icon to API

--- src/app.js
+++ src/app.js
@@ -29,16 +29,22 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let winndElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   winndElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.getAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "2b6fdad0cbd018949c50c70f72250726";
-let city = "London";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(`${apiUrl}&appid=${apiKey}`).then(displayTemperature);
